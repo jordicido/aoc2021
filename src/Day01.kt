42a775bfa97d92ba73e99836f9e06212d7590916
Jordi Cidoncha <jordi.cidoncha@itb.cat>
2021-12-01 10:00:17 +0100

    Day one

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var res = 0
+        for (i in 1 until input.size) {
+            if (input[i] > input[i-1]) res++
+        }
+        return res
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var res = 0
+        for (i in 3 until input.size) {
+            val firstThree = input[i-3] + input[i-2] + input[i-1]
+            val secondThree = input[i-2] + input[i-1] + input[i]
+            if (secondThree > firstThree) res++
+        }
+        return res
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readInput("day1")
+    input.map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
