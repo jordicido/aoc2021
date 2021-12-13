b0740d56de8cad6529f16583fe67e25ce1c37da2
Jordi Cidoncha <jordi.cidoncha4@gmail.com>
2021-12-13 21:02:08 +0100

    Update Day12.kt

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,38 +1,26 @@
 fun main() {
-    var waysOfstart: MutableList<String> = mutableListOf()
+    var waysOfStart: MutableList<String> = mutableListOf()
     var stackToVisit: MutableList<String> = mutableListOf()
-    var stackVisited: MutableList<String> = mutableListOf()
     var input: List<List<String>> = mutableListOf()
 
-    fun lookForPath(way: String): Int {
-        return if (way == "end") {
-            stackVisited = mutableListOf()
-            1
-        }
-//        else if (stackVisited.contains(way) && way[0].isLowerCase()) 0
+    fun lookForPath(way: String, visited: MutableList<String>): Int {
+        return if (way == "end") 1
         else {
-            var hasWays: MutableList<String> = mutableListOf()
-            stackVisited.add(way)
-            input.filter { it[0] == way }.forEach { dest -> hasWays.add(dest[1]) }
-            if (hasWays.isNotEmpty()) stackToVisit.addAll(hasWays)
-            else input = input.filter { it[1] != way }
-            if (stackToVisit.isNotEmpty()) lookForPath(stackToVisit.removeAt(0))
-            else {
-                stackVisited = mutableListOf()
-                0
-            }
+            if (way[0].isLowerCase()) visited.add(way)
+            input.filter { it[0] == way }.forEach { dest -> if (!visited.contains(dest[1])) stackToVisit.add(dest[1]) }
+            if (stackToVisit.isNotEmpty()) lookForPath(stackToVisit.removeAt(0), visited)
+            else 0
         }
     }
 
     fun part1(): Int {
         var res = 0
 
-        input.filter { it[0] == "start" }.forEach { dest -> waysOfstart.add(dest[1]) }
+        input.filter { it[0] == "start" }.forEach { dest -> waysOfStart.add(dest[1]) }
 
-        while (waysOfstart.isNotEmpty()) {
-            res += lookForPath(waysOfstart.removeAt(0))
+        while (waysOfStart.isNotEmpty()) {
+            res += lookForPath(waysOfStart.removeAt(0), mutableListOf())
             stackToVisit = mutableListOf()
-            stackVisited = mutableListOf()
         }
 
         return res
@@ -51,3 +39,4 @@ fun main() {
     println(part1())
     println(part2())
 }
+
